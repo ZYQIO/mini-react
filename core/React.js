5788418473fee960c277f2b4aa042e732e462115
dt_2508806787 <13477681517@163.com>
2024-04-21 22:27:01 +0800

    feat: 实现fiber架构

--- core/React.js
+++ core/React.js
@@ -22,23 +22,97 @@ function createElement(type, props, ...children) {
 }
 
 function render(el, container) {
-    const dom = el.type === 'TEXT_ELEMENT' ? document.createTextNode("") : document.createElement(el.type);
 
-    // id class
-    Object.keys(el.props).forEach(key => {
-        if (key !== 'children') {
-            dom[key] = el.props[key]
+    nextWorkOfUnit = {
+        dom: container,
+        props: {
+            children: [el]
         }
-    })
+    }
 
-    const children = el.props.children;
+    // const dom = el.type === 'TEXT_ELEMENT' ? document.createTextNode("") : document.createElement(el.type);
 
-    children.forEach(child => {
-        render(child, dom)
-    })
+    // // id class
+    // Object.keys(el.props).forEach(key => {
+    //     if (key !== 'children') {
+    //         dom[key] = el.props[key]
+    //     }
+    // })
 
-    container.append(dom)
+    // const children = el.props.children;
+
+    // children.forEach(child => {
+    //     render(child, dom)
+    // })
+
+    // container.append(dom)
 }
+
+let nextWorkOfUnit = null
+function workLoop(deadline) {
+
+    let shouldYield = false;
+    while (!shouldYield && nextWorkOfUnit) {
+        nextWorkOfUnit = performnextWorkOfUnit(nextWorkOfUnit);
+
+        shouldYield = deadline.timeRemaining() < 1;
+    }
+
+    requestIdleCallback(workLoop)
+}
+
+function performnextWorkOfUnit(work) {
+    if (!work.dom) {
+        // 1. 创建dom
+        const dom = work.dom = work.type === 'TEXT_ELEMENT' ? document.createTextNode("") : document.createElement(work.type);
+        // debugger
+
+        console.log('ork.parent.dom', work.parent.dom);
+
+        work.parent.dom?.append(dom)
+
+        // 2. 处理props
+        // id class
+        Object.keys(work.props).forEach(key => {
+            if (key !== 'children') {
+                dom[key] = work.props[key]
+            }
+        })
+    }
+
+    // 3. 转换链表 设置好指针
+    const children = work.props.children;
+    let prevChild = null;
+    children.forEach((child, index) => {
+        const newWork = {
+            type: child.type,
+            props: child.props,
+            child: null,
+            parent: work,
+            sibling: null,
+            dom: null
+        }
+        if (index === 0) {
+            work.child = newWork
+        } else {
+            prevChild.sibling = newWork
+        }
+        prevChild = newWork
+    })
+
+    // 4. 返回下一个要执行的任务
+    if (work.child) {
+        return work.child
+    }
+
+    if (work.sibling) {
+        return work.sibling
+    }
+
+    return work.parent?.sibling
+}
+
+requestIdleCallback(workLoop)
 
 const React = {
     render,
